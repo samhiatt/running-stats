7a576ae35c549f9e6651d39542746764f483a59b
Sam Hiatt <sam.hiatt@weather.com>
2015-03-18 19:42:23 -0700

    Added tests for initialization with values.

--- tests.js
+++ tests.js
@@ -46,10 +46,38 @@ exports.testCircStats = function(test) {
 	test.done();
 };
 
+exports.testStatsInitializationWithInitValues = function(test) {
+	var s = new running_stats.Stats(3,2);
+	s.push(6);
+
+	var s2 = new running_stats.Stats(s);
+	test.equals(s2.mean,4,"Mean = 4");
+	test.equals(s2.max,6,"Max = 6");
+	test.equals(s2.min,3,"Min = 3");
+	test.equals(s2.variance,2,"Variance = 2");
+	test.equals(s2.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
+	test.equals(s2.count,3,"Count = 3");
+
+	test.done();
+};
+
 exports.testInitializationWithCircStats = function(test) {
 	var s = new running_stats.CircStats();
 	s.push(0);
 	s.push(360);
+
+	var cs = new running_stats.CircStats(s);
+	cs.push(90,2);
+
+	test.ok(cs.mean-45<0.000000001, "Mean = 45");
+	test.equals(cs.variance, 0.29289321881345254, "Variance = 0.29289321881345254");
+	test.equals(cs.count, 4, "Count = 4");
+
+	test.done();
+};
+
+exports.testCircStatsInitializationWithInitValues = function(test) {
+	var s = new running_stats.CircStats(360,2);
 
 	var cs = new running_stats.CircStats(s);
 	cs.push(90,2);
