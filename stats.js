eb4d84f08ad133eb7cc770dce42b18a07cd164d1
Sam Hiatt <sam.hiatt@weather.com>
2015-03-18 19:00:48 -0700

    Enabling initialization with stats object

--- stats.js
+++ stats.js
@@ -1,28 +1,26 @@
-module.exports.Stats = function() {
+module.exports.Stats = function(val, weight) {
 	this.count=0;
 	this.mean=null;
 	this.min=null;
 	this.max=null;
 	this.variance=0;
 	this.stdDev=0;
-	this._v=0;
 	this.push = function (val,weight) {
 		if (val==null) return;
 		weight = (weight!=null)? weight : 1;
-		//if (typeof val.mean != 'undefined') {
-		//	weight = val.count;
-		//	if (this.max==null || val.max>this.max) this.max = val.max;
-		//	if (this.min==null || val.min<this.min) this.min = val.min;
-		//	this.variance = val.variance;
-		//	val = val.mean;
-		//}
+		if (typeof val.mean != 'undefined') {
+			weight = val.count;
+			if (this.max==null || val.max>this.max) this.max = val.max;
+			if (this.min==null || val.min<this.min) this.min = val.min;
+			this.variance = val.variance;
+			val = val.mean;
+		}
 		var oldMean = this.mean || 0;
 		this.count += weight;
 		if (this.max==null || val>this.max) this.max = val;
 		if (this.min==null || val<this.min) this.min = val;
 		this.mean = (this.count>0)?  oldMean + (val-oldMean)*weight/this.count : val;
-		this._v = this._v + weight*(val-oldMean)*(val-this.mean);
-		this.variance = this._v/this.count;
+		this.variance = (this.variance*this.count + weight*(val-oldMean)*(val-this.mean))/this.count;
 		this.stdDev = Math.sqrt(this.variance);
 		return this;
 	};
@@ -36,4 +34,5 @@ module.exports.Stats = function() {
 			count: this.count
 		};
 	};
+	if (typeof val != 'undefined') this.push(val, weight);
 };
--- tests.js
+++ tests.js
@@ -16,3 +16,19 @@ exports.testStats = function(test){
 	test.done();
 
 };
+
+exports.testInitializationWithStats = function(test) {
+	var s = new running_stats.Stats();
+	s.push(6);		// push val 6, with implied weight of 1
+	s.push(3, 2);	// push val 3, with weight of 2
+
+	var s2 = new running_stats.Stats(s);
+	test.equals(s2.mean,4,"Mean = 4");
+	test.equals(s2.max,6,"Max = 6");
+	test.equals(s2.min,3,"Min = 3");
+	test.equals(s2.variance,2,"Variance = 2");
+	test.equals(s2.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
+	test.equals(s2.count,3,"Count = 3");
+
+	test.done();
+};
