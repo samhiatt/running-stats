4825407d462c3774b022d066ffed7147aa91015b
Sam Hiatt <sam.hiatt@gmail.com>
2015-03-18 23:46:37 -0700

    Adds ability to add by array.

--- tests/StatsTest.js
+++ tests/StatsTest.js
@@ -1,19 +1,29 @@
 var running_stats = require('./../lib/index');
 
+var checkResult = function(test,res){
+	test.equals(res.mean,4,"Mean = 4");
+	test.equals(res.max,6,"Max = 6");
+	test.equals(res.min,3,"Min = 3");
+	test.equals(res.variance,2,"Variance = 2");
+	test.equals(res.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
+	test.equals(res.count,3,"Count = 3");
+	test.done();
+};
+
 exports.testStats = function(test){
 
 	var s = new running_stats.Stats();
-	s.push(6);		// push val 6, with implied weight of 1
-	s.push(3, 2);	// push val 3, with weight of 2
+	s.push(6);
+	s.push(3);
+	s.push(3);
+	checkResult(test, s);
 
-	test.equals(s.mean,4,"Mean = 4");
-	test.equals(s.max,6,"Max = 6");
-	test.equals(s.min,3,"Min = 3");
-	test.equals(s.variance,2,"Variance = 2");
-	test.equals(s.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
-	test.equals(s.count,3,"Count = 3");
+};
 
-	test.done();
+exports.testStatsInitWithList = function(test){
+
+	var s = new running_stats.Stats([6,3,3]);
+	checkResult(test, s);
 
 };
 
@@ -21,29 +31,13 @@ exports.testInitializationWithStats = function(test) {
 	var s = new running_stats.Stats();
 	s.push(6);		// push val 6, with implied weight of 1
 	s.push(3, 2);	// push val 3, with weight of 2
-
 	var s2 = new running_stats.Stats(s);
-	test.equals(s2.mean,4,"Mean = 4");
-	test.equals(s2.max,6,"Max = 6");
-	test.equals(s2.min,3,"Min = 3");
-	test.equals(s2.variance,2,"Variance = 2");
-	test.equals(s2.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
-	test.equals(s2.count,3,"Count = 3");
-
-	test.done();
+	checkResult(test, s2);
 };
 
 exports.testStatsInitializationWithInitValues = function(test) {
 	var s = new running_stats.Stats(3,2);
 	s.push(6);
-
 	var s2 = new running_stats.Stats(s);
-	test.equals(s2.mean,4,"Mean = 4");
-	test.equals(s2.max,6,"Max = 6");
-	test.equals(s2.min,3,"Min = 3");
-	test.equals(s2.variance,2,"Variance = 2");
-	test.equals(s2.stdDev,1.4142135623730951,"StdDev = 1.4142135623730951");
-	test.equals(s2.count,3,"Count = 3");
-
-	test.done();
+	checkResult(test, s2);
 };
